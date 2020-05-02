82f1c18ec12be9b0919b1de64efc752fa386bd6c
Oliver <oliversiedler@gmx.de>
2020-05-02 10:37:49 +0200

    answers are represented as tree structure

--- Backend/Entities/Models/DiscussionEntry.cs
+++ Backend/Entities/Models/DiscussionEntry.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -7,9 +8,13 @@ namespace Entities.Models
     [Table("Discussion_Entry")]
     public class DiscussionEntry : IEntity
     {
+        public DiscussionEntry()
+        {
+            Answers = new HashSet<DiscussionEntry>();
+        }
+
         [Key]
         public long Id { get; set; }
-        public long? AnswerTo { get; set; }
         public string Topic { get; set; }
         public string Text { get; set; }
         public DateTime? TimeStamp { get; set; }
@@ -17,7 +22,12 @@ namespace Entities.Models
         public long? NormalGroup { get; set; }
 
         public long? UserId { get; set; }
-   
+
+        public long? AnswerTo { get; set; }
+        [ForeignKey("AnswerTo")]
+        public DiscussionEntry ParentDiscussionEntry { get; set; }
+        public ICollection<DiscussionEntry> Answers { get; set; }
+
         public long? FileId { get; set; }
         [ForeignKey("FileId")]
         public File File { get; set; }
