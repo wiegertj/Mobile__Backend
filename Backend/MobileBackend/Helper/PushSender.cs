a41966bb357ccfa721f0b4adf9cdfba29a6966b0
Oliver <oliversiedler@gmx.de>
2020-06-06 17:58:33 +0200

    changed lang

--- Backend/MobileBackend/Helper/PushSender.cs
+++ Backend/MobileBackend/Helper/PushSender.cs
@@ -66,8 +66,8 @@ namespace Mobile_Backend.Helper
 
             byte[] byteArray = Encoding.UTF8.GetBytes("{"
                                                     + "\"app_id\": \"71f3050c-2a04-4d17-81ec-33f76361bf19\","
-                                                    + $"\"headings\":{{\"de\":\"{title}\"}},"
-                                                    + $"\"contents\":{{\"de\": \"{text}\"}},"
+                                                    + $"\"headings\":{{\"en\":\"{title}\"}},"
+                                                    + $"\"contents\":{{\"en\": \"{text}\"}},"
                                                     + $"\"data\":{data},"
                                                     + $"\"filters\": [{filter}]}}");
 
